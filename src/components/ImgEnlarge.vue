5e3bb1055e3b0ae014a5b971943709d97afbea78
Zhang Long <979463796@qq.com>
2019-08-22 18:02:56 +0800

    feat: 图片放大相关参考文档

--- src/components/ImgEnlarge.vue
+++ src/components/ImgEnlarge.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
+    <!-- https://www.jb51.net/article/133409.htm -->
+    <!-- https://github.com/826327700/vue-piczoom/blob/master/src/components/PicZoom.vue -->
     <p>图片放大---vue-piczoom</p>
     <!-- <div class="magnifier-box" :class="vertical?'vertical':''" :ref="id" @mousemove="mousemove" @mouseover="mouseover" @mouseleave="mouseleave">
         <img v-show="showImg" :src="imgUrl" alt="">
