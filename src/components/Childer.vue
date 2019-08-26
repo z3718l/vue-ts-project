977487571a616d09161b6fb16e977daa81e61ef5
zhanglong <979463796@qq.com>
2019-08-26 21:54:48 +0800

    组件之间传值有bug

---
+++ src/components/Childer.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div>子组件</div>
+  </div>
+</template>
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator'
+@Component({
+  components: {},
+  // props: {
+  //   title: {
+  //     type: String,
+  //     default: ''
+  //   }
+  // }
+})
+export default class Childer extends Vue {
+  // @Prop() title: String
+  // props: ['title']
+  mounted () {
+    console.log('生命周期---counted')
+    // console.log()
+  }
+}
+</script>
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,6 +1,18 @@
 <template>
   <div>
     <div class="title">登录页</div>
+    <Childer title="标题"></Childer>
   </div>
 </template>
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+import Childer from './Childer.vue'
+@Component({
+  components: {
+    Childer
+  }
+})
+export default class Login extends Vue {
 
+}
+</script>
